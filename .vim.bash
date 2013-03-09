9c3f3a8945877e705249ed8290cc221b4d0e47e1
tom <tom@x2k.co.uk>
2013-03-09 12:56:47 +0000

    Added comments

--- .vim.bash
+++ .vim.bash
@@ -1,5 +1,6 @@
 vim()
 {
+    # Remap Escape to caps lock
     echo "keycode 66 = Caps_Lock
         keycode 9 = Escape
         remove Lock = Caps_Lock
@@ -7,12 +8,17 @@ vim()
         keycode 9 = Caps_Lock
         keycode 66 = Escape" | xmodmap -
 
+    # Save origin terminal settings - so went can remap ctrl-s
     local STTYOPTS="$(stty --save)"
     stty stop '' -ixoff
 
+    # Run vim
     command vim "$@"
+
+    # Restore terminal settings
     stty "$STTYOPTS"
 
+    # Restore caps lock
     echo "keycode 66 = Caps_Lock
         keycode 9 = Escape
         add Lock = Caps_Lock
